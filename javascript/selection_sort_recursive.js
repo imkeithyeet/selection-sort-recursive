4fcd829919e0f2abbb2cef94ef418b3513e9da21
Keith Ma <keithchezzy@gmail.com>
2023-01-01 21:32:55 -0800

    sorting

--- javascript/selection_sort_recursive.js
+++ javascript/selection_sort_recursive.js
@@ -1,7 +1,45 @@
-function selectionSortRecursive(arr) {
-  // type your code here
-}
+// function selectionSortRecursive(arr) {
+  // *iteration way*
+// const sorted = [];
+// while (arr.length > 0){
+//   const min = Math.min(...arr);
+//   const idx = arr.indexOf(min);
 
+//   sorted.push(min);
+//   arr.splice(idx, 1);
+// }
+// return sorted 
+//   // type your code here
+// }
+// *second recursive way
+function selectionSortRecursive(arr) {
+  if (arr.length === 0){
+    return [];
+  }
+  const min = Math.min(...arr);
+  const idx = arr.indexOf(min);
+  arr.splice(idx, 1);
+
+  const result = selectionSortRecursive(arr);
+  result.unshift(min);
+  return result;
+}
+// *third recursive way*
+// function selectionSortRecursive(arr) {
+//   if (arr.length === 0){
+//     return arr
+//   }
+//   let minIndex = 0
+//   for (let i = 1; i < arr.length; i++){
+//     if (arr[i] < arr[minIndex]){
+//       minIndex = i;
+//     }
+//   }
+//   let temp = arr[0]
+//   arr[0] = arr[minIndex]
+//   arr[minIndex] = temp 
+//   return [arr[0]].concat(selectionSortRecursive(arr.slice(1)));
+// }
 if (require.main === module) {
   // add your own tests in here
   console.log("Expecting: [-1, 2, 3, 5]");
